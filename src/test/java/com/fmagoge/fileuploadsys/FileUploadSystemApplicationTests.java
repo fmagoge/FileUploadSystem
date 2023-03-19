4dc03d7725ac2e6cc68b22f172a58f0fb25e751e
fmagoge <fmagoge@gmail.com>
2023-03-20 00:04:43 +0200

    Initial commit

---
+++ src/test/java/com/fmagoge/fileuploadsys/FileUploadSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmagoge.fileuploadsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
