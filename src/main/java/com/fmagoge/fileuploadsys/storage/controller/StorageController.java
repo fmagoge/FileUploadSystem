228270514108d6740d9d6bd2b06905f7ce8bda72
fmagoge <fmagoge@gmail.com>
2023-03-24 13:58:21 +0200

    Update StorageController.java
    unused imports and commented out code

--- src/main/java/com/fmagoge/fileuploadsys/storage/controller/StorageController.java
+++ src/main/java/com/fmagoge/fileuploadsys/storage/controller/StorageController.java
@@ -1,24 +1,13 @@
 package com.fmagoge.fileuploadsys.storage.controller;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fmagoge.fileuploadsys.storage.exception.StorageFileNotFoundException;
@@ -39,22 +28,8 @@ public class StorageController {
 	@GetMapping("/")
 	public String listUploadedFiles() {
 
-//		model.addAttribute("files", service.loadAll().map(
-//				path -> MvcUriComponentsBuilder.fromMethodName(StorageController.class,
-//						"serveFile", path.getFileName().toString()).build().toUri().toString())
-//				.collect(Collectors.toList()));
-
 		return "uploadForm";
 	}
-
-//	@GetMapping("/files/{filename:.+}")
-//	@ResponseBody
-//	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-//
-//		Resource file = service.loadAsResource(filename);
-//		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-//				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-//	}
 
 	@PostMapping("/")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
